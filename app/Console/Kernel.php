71cfe1af8050b43f562e3b4f16762cf459bfcd2e
Ioar <crespo.116239@e.unavarra.es>
2019-04-02 16:37:40 +0200

    https5

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
         //
     ];
 
-    protected $middleware = [
+    /*protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
  
         //custom middleware for https redirect
         \App\Http\Middleware\HttpsMiddleware::class
-    ];
+    ];*/
 
     /**
      * Define the application's command schedule.
