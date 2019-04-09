617d60f62eeaa37a7614decd1037ea81022649f3
Ospino <ospino.p@gmail.com>
2019-04-09 18:41:22 +0200

    imgs
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,21 @@ Auth::routes();
 Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+
+Route::get('storage/{filename}', function ($filename)
+{
+    $path = storage_path('public/' . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
