9641f529dcab9de23dbbc331d0114ef9d4771b86
Ospino <ospino.p@gmail.com>
2019-05-12 20:42:15 +0200

    Ranking changed to only show one image
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- resources/views/posts/ranking.blade.php
+++ resources/views/posts/ranking.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Top 10 posts</h1>
-    <h4>These are the most liked posts in Photoshare</h4>
+    <h1>Top 5</h1>
     @if (count($posts) > 0)
         <ul class="list-group">
         @foreach ($posts as $post)
             <li class="list-group-item">
                 <div class="well text-center">
                     <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
-                    @foreach ($post->getMedia('document') as $media)
-                    {{-- <div> --}}
+                    <h6>Likes {{$post->likes}}</h6>
+                    @if (count($post->getMedia('document')) > 0)
+                      <img class="rounded mx-auto" src='{{$post->getMedia('document')[0]->getUrl()}}' width="20%" height="20%">
+                    @endif
+
+                    {{-- @foreach ($post->getMedia('document') as $media)
                         <img class="rounded mx-auto" src='{{$media->getUrl()}}' width="20%" height="20%">
-                    {{-- </div> --}}
-                    @endforeach                                     
+                    @endforeach --}}
                 </div>
                 <div class="text-center">Posted by {{$post->user->name}}</div>
             </li>
